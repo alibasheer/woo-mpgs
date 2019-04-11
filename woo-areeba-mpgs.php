4ac03046fe7c103b9d725d688648e315f3919c15
ALi BACHIR <alibasheer@hotmail.com>
2019-04-11 21:19:30 +0300

    added class to extend WC payment gateway

--- woo-areeba-mpgs.php
+++ woo-areeba-mpgs.php
@@ -21,3 +21,34 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 	return;
 }
+
+/**
+ * Add the gateway to WC Available Gateways
+ *
+ * @since 1.0.0
+ * @param array $gateways all available WC gateways
+ * @return array $gateways all WC gateways + Areeba MPGS gateway
+ */
+function wc_areeba_mpgs_add_to_gateways( $gateways ) {
+	$gateways[] = 'WC_Areeba_MPGS';
+	return $gateways;
+}
+add_filter( 'woocommerce_payment_gateways', 'wc_areeba_mpgs_add_to_gateways' );
+
+/**
+ * WooCommerce Areeba MPGS
+ *
+ * Extends WooCommerce with Areeba MasterCard Payment Gateway Services (MPGS).
+ *
+ * @class 		WC_Areeba_MPGS
+ * @extends		WC_Payment_Gateway
+ * @version		1.0.0
+ * @package		WooCommerce/Classes/Payment
+ * @author 		Ali Basheer
+ */
+function wc_areeba_mpgs_init() {
+	class WC_Areeba_MPGS extends WC_Payment_Gateway {
+
+	}
+}
+add_action( 'plugins_loaded', 'wc_areeba_mpgs_init', 11 );
