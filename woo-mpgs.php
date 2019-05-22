5e45a26c5e25bf98fd81fcb0036111ae5e278c5c
ALi BACHIR <alibasheer@hotmail.com>
2019-05-23 00:47:05 +0300

    Multisite compatibility: changed the way to check if woocommerce exit

--- woo-mpgs.php
+++ woo-mpgs.php
@@ -18,7 +18,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /*
  *  Make sure WooCommerce is active
  */
-if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+if ( ! class_exists( 'woocommerce' ) ) {
 	return;
 }
 
